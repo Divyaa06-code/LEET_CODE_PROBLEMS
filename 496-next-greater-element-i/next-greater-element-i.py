1228fa39fa185d3c23b20190a2c2df9211c3f028
Divyaa Sri S <sripathyg60032@gmail.com>
2026-07-12 23:53:03 +0530

    Time: 3 ms (53.17%) | Memory: 12.5 MB (91.73%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        stack = []
+        d = {}
+
+        for n in nums2:
+            while stack and stack[-1] < n:
+                d[stack.pop()] = n
+            stack.append(n)
+
+        return [d.get(x, -1) for x in nums1]
